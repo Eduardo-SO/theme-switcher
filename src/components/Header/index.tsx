7b07d3bd1d94131978450fdfc9445b0a73c14168
Eduardo SO <eduardo.sool07@gmail.com>
2020-08-01 22:45:02 -0300

    add: Theme switcher

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from 'styled-components';
+import { shade } from 'polished';
+import Switch from 'react-switch';
 
 import { Container } from './styles';
 
-const Header: React.FC = () => {
-  return <Container>Hello dude!</Container>;
+interface HeaderProps {
+  toggleTheme(): void;
+}
+
+const Header: React.FC<HeaderProps> = ({ toggleTheme }) => {
+  const { colors, title } = useContext(ThemeContext);
+
+  return (
+    <Container>
+      <h2>Hello dude!</h2>
+      <Switch
+        onChange={toggleTheme}
+        checked={title === 'dark'}
+        checkedIcon={false}
+        uncheckedIcon={false}
+        height={10}
+        width={40}
+        handleDiameter={20}
+        offColor={shade(0.5, colors.background)}
+        onColor={colors.secondary}
+      />
+    </Container>
+  );
 };
 
 export default Header;
---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#333',
+    secondary: '#03045e',
+
+    background: '#222',
+    text: '#fff',
+  },
+};
